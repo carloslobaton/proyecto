835d5df3ea6ec9d70d4b3b0cdaaf1dc795361080
carlos <clobaton@topstrategic.net>
2025-12-14 12:05:37 -0500

    cambio en cadena de conexion a postgress

--- LPl_T2_FloresAedoMery/src/main/java/pe/cibertec/streaming/config/DBConnection.java
+++ LPl_T2_FloresAedoMery/src/main/java/pe/cibertec/streaming/config/DBConnection.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
+/*
     // 👇 OJO: tu BD se llama bd_avalos (según tus capturas)
     private static final String URL =
             "jdbc:mysql://localhost:3306/bd_flores"
@@ -22,6 +23,29 @@ public class DBConnection {
             Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL Connector/J moderno
         } catch (ClassNotFoundException e) {
             throw new ExceptionInInitializerError("No se pudo cargar el driver MySQL: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+	*/
+	
+	// 📌 Datos de PostgreSQL (Render)
+    private static final String URL =
+            "jdbc:postgresql://dpg-d4vencnpm1nc73bn7cb0-a.virginia-postgres.render.com:5432/bdflores";
+
+    private static final String USER = "bdflores_user";
+    private static final String PASS = "klCEMQlTBCJZmzbOV7uwSbanqngqBf3f";
+
+    static {
+        try {
+            // ✅ Driver PostgreSQL
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError(
+                    "No se pudo cargar el driver PostgreSQL: " + e.getMessage()
+            );
         }
     }
 
